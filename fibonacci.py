e5849bf622c98b17cfdb13af6efbbe456f48dbb5
harilme <70759292+harilme@users.noreply.github.com>
2023-08-07 14:32:04 +0100

    Create fibonacci.py

---
+++ fibonacci.py
@@ -0,0 +1,19 @@
+def generate_fibonacci(limit):
+    fibonacci_sequence = [0, 1]  # Initialize with the first two Fibonacci numbers
+    
+    while True:
+        next_number = fibonacci_sequence[-1] + fibonacci_sequence[-2]
+        if next_number > limit:
+            break
+        fibonacci_sequence.append(next_number)
+    
+    return fibonacci_sequence
+
+# Input from the user
+limit = int(input("Enter a positive integer: "))
+
+if limit <= 0:
+    print("Please enter a positive integer.")
+else:
+    fibonacci_result = generate_fibonacci(limit)
+    print("Fibonacci sequence up to", limit, ":", fibonacci_result)
